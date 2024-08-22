83972750edcece79fe60343dcd75e91821e9cec5
Juan Esteban Vallejo Hincapie <juanvallejo240@gmail.com>
2024-08-22 11:17:31 -0500

    metodos nuevos

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,10 +1,22 @@
 class Todo:
+
     def __int__(self, code_id: int, title :str, description :str):
         self.code_id: int = code_id
         self.title: str = title
         self.description: str = description
         self.completed: bool = False
         self.tags : list[str] = []
+
+    def mark_completed(self):
+        self.completed: bool = True
+
+    def add_tag(self, tag: str):
+        if tag in self.tags:
+            return self.tags
+        else:
+            return self.tags.append(tag)
+
+
 
 
 
