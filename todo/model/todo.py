c8156146dba8bb41b6d56e36743ae5663aca9ede
Juan Esteban Vallejo Hincapie <juanvallejo240@gmail.com>
2024-08-22 11:28:50 -0500

    metodos nuevo y nueva clae con su metodo inicializador

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,14 +1,14 @@
 class Todo:
 
-    def __int__(self, code_id: int, title :str, description :str):
+    def __int__(self, code_id: int, title:str, description:str):
         self.code_id: int = code_id
         self.title: str = title
         self.description: str = description
         self.completed: bool = False
-        self.tags : list[str] = []
+        self.tags: list[str] = []
 
     def mark_completed(self):
-        self.completed: bool = True
+        self.completed = True
 
     def add_tag(self, tag: str):
         if tag in self.tags:
@@ -16,7 +16,13 @@ class Todo:
         else:
             return self.tags.append(tag)
 
+    def __str__(self) -> str:
+        return f"{self.code_id} - {self.title}"
 
 
+class TodoBook:
+
+    def __int__(self):
+        self.todos: dict() = {}
 
 
